e0611973ad6e42927491b54c16e280f548cbe9bf
Seungah Lim <sss.lim@samsung.com>
2025-07-24 15:21:40 +0900

    [feature] implement read

--- Project11/device_driver.cpp
+++ Project11/device_driver.cpp
@@ -1,4 +1,9 @@
 #include "device_driver.h"
+#include <stdexcept>
+
+class ReadFailException : public std::exception {
+
+};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {
@@ -6,8 +11,14 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int result = (int)(m_hardware->read(address));  // 1st read
+    for (int i = 1; i < 5; ++i) {                   // additionl 4 times of read
+        int current = (int)(m_hardware->read(address));
+        if (current != result) {
+            throw ReadFailException();
+        }
+    }
+    return result;
 }
 
 void DeviceDriver::write(long address, int data)
--- Project11/main.cpp
+++ Project11/main.cpp
@@ -1,12 +1,22 @@
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
+using namespace testing;
+
+class MockFlashMemory : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
 TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
+	MockFlashMemory mockHardware;
+	DeviceDriver driver{ &mockHardware };
+
+	EXPECT_CALL(mockHardware, read(_))
+		.Times(5);
+
 	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
 }
 
 int main() {
